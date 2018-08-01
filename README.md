f41191e401c7faabc854969636e4a064e78ddad3
Tyler Haas <Tyler.Haas@roomtoread.org>
2018-07-31 17:08:51 -0700

    Inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stockify
+A simple API wrapper for stock data using the [AlphaVantage API](https://www.alphavantage.co/documentation/)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Stockify',
+      version='0.1',
+      description='A simple API wrapper for Stock data',
+      url='https://github.com/haaspt/Stockify',
+      author='Patrick Tyler Haas',
+      author_email='patrick.tyler.haas@gmail.com',
+      license='MIT',
+      packages=['Stockify'],
+      zip_safe=False)

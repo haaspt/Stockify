abfd7be27229a4128c2dab97a218b1f5ebbf7ccb
Patrick Tyler Haas <patrick.tyler.haas@gmail.com>
2018-08-03 14:28:18 -0700

    Initial commit

---
+++ tests/portfolio_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+import Stockify
+
+class PortfolioTest(unittest.TestCase):
+
+    def test_holding(self):
+
+        holding = Stockify.Holding('aapl')
+        price = holding.get_price()
+        expected_price = Stockify.Data.price('aapl')
+        self.assertEqual(expected_price, price, ('Expected price did not match '
+                                                 f'returned price: {price}'))
+    
+    def test_portfolio(self):
+
+        portfolio = Stockify.Portfolio()
+        symbol1 = 'aapl'
+        symbol2 = 'v'
+        portfolio.add_holding(symbol1)
+        portfolio.add_holding(symbol2)
+        self.assertEqual(2, len(portfolio.holdings))
+        self.assertEqual('AAPL', portfolio['AAPL'].symbol)
+        

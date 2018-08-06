cd5e93805e9c9aad5daf6211b906611a01829e9a
Tyler Haas <Tyler.Haas@roomtoread.org>
2018-08-06 11:08:09 -0700

    Add: portfolio test coverage

--- tests/portfolio_test.py
+++ tests/portfolio_test.py
@@ -19,8 +19,18 @@ class PortfolioTest(unittest.TestCase):
         symbol2 = 'v'
         portfolio.add_holding(symbol1)
         portfolio.add_holding(symbol2)
-        self.assertEqual(2, len(portfolio.holdings))
+        self.assertEqual(2, len(portfolio))
         self.assertEqual('AAPL', portfolio['AAPL'].symbol)
+        portfolio.add_holdings(['ms', 'fb'])
+        self.assertEqual(4, len(portfolio))
+
+        my_holdings = ['aapl', 'v', 'ms']
+        portfolio_with_holdings = Stockify.Portfolio(my_holdings)
+        number_of_holdings = len(portfolio_with_holdings)
+        self.assertEqual(4, number_of_holdings)
+        # Value returns 0.0 because no lots have been added
+        self.assertEqual(0.0, portfolio_with_holdings.get_value())
+        self.assertEqual(4, len(portfolio_with_holdings.get_prices()))
 
 
 if __name__ == '__main__':
